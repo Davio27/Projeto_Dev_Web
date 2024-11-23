265bad8013abecddd99fba79e374c7c8d49ca8a7
Dávio Carvalho <129198119+Davio27@users.noreply.github.com>
2024-11-23 19:45:05 -0300

    Update autentication.js

--- config/autentication.js
+++ config/autentication.js
@@ -9,24 +9,3 @@ export function validateLogin(email, password) {
         return false; // Credenciais incorretas
     }
 }
-
-// // Função para configurar o comportamento da modal
-// export function setupModal() {
-//     const loginModal = document.getElementById('loginModal');
-//     const closeLogin = document.querySelector('.closelogin');
-
-//     closeLogin.addEventListener('click', () => {
-//         loginModal.style.display = 'none'; // Fecha a modal
-//     });
-
-//     const showPasswordCheckbox = document.getElementById('showPasswordCheckbox');
-//     const passwordInput = document.getElementById('loginPassword');
-    
-//     showPasswordCheckbox.addEventListener('change', function () {
-//         if (this.checked) {
-//             passwordInput.type = 'text';
-//         } else {
-//             passwordInput.type = 'password';
-//         }
-//     });
-// }
