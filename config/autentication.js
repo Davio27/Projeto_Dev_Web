ffe86a7c3c82cfcb2853b046599dc8baf99fad09
Dávio Carvalho <129198119+Davio27@users.noreply.github.com>
2024-11-25 19:35:23 -0300

    Update autentication.js

--- config/autentication.js
+++ config/autentication.js
@@ -3,7 +3,7 @@ export function validateLogin(email, password) {
     const correctEmail = 'daviccarvalho11@hotmail.com';
     const correctPassword = '12345abcd';
 
-    if (email === correctEmail && password === correctPassword) {
+    if (email.toLowerCase() === correctEmail && password === correctPassword) {
         return true; // Login bem-sucedido
     } else {
         return false; // Credenciais incorretas
